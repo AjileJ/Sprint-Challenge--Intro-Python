60562de62190568ec467de37abe0a7b0c156668f
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-02-14 12:30:59 -0800

    mvp completed

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -6,8 +6,8 @@ class City:
     self.name = name
     self.lat = lat
     self.lon = lon
-    def __str__(self):
-      return f"City({self.name}, {self.lat} , {self.lon})"
+  def __str__(self):
+    return f"{self.name}, {self.lat} , {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -24,20 +24,23 @@ class City:
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-  with open('cities.csv', newline='') as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for row in csvReader:
-        cities.append(City(row['city'], float(row['lat']), float(row['lng'])))
-    return cities[1:]
+
+
+    with open('cities.csv', newline='') as csvFile:
+
+      csvReader = csv.reader(csvFile)
+      next(csvReader)
+      for row in csvReader:
+        cities.append(City(str(row[0]), float(row[3]), float(row[4])))
+    return cities
 
 cityreader(cities)
 
 
 for c in cities:
-  print(f"City(\"{c.name}\", {c.lat}, {c.lon})")
+  #print(f"City(\"{c.name}\", {c.lat}, {c.lon})")
+  print(c)
+  
 
 
 
