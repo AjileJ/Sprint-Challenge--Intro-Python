fa57b332d7046e6e0983afa121638510d9c4709d
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-02-14 09:03:55 -0800

    city reader looking alright/usr/local/bin/python3 /Users/jathey/Documents/Sprint-Challenge--Intro-Python/src/cityreader/test_cityreader.py

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,13 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+    def __str__(self):
+      return f"City({self.name}, {self.lat} , {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,14 +27,19 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open('src/cityreader/cities.csv', newline='') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',', quotechar= '|')
+    for row in csvReader:
+        cities.append(City(row[0], row[3], row[4]))
+  
     
     return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-    print(c)
+print([f"City{c.name}, {c.lat}, {c.lon}" for c in cities])
+    
 
 # STRETCH GOAL!
 #
